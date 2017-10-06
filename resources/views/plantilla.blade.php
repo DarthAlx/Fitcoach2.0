5e54bb4b3ab0a5ae2a2970bb2699d02f4da31efd
Alexis Morales <alx.morales@hadoukendev.com>
2017-10-06 14:12:26 -0500

    responsivo 3

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -53,6 +53,27 @@
   gtag('config', 'UA-107673883-1');
 </script>
 
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{
+	if(f.fbq)return;n=f.fbq=function(){
+	n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)
+};
+if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0
+n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '141416126593810');
+fbq('track', 'PageView');
+</script>
+<noscript>
+<img height="1" width="1" src="https://www.facebook.com/tr?id=141416126593810&ev=PageView&noscript=1"/>
+</noscript>
+<!-- End Facebook Pixel Code -->
+
 	</head>
 <body class="home">
 	<div class="pagecontainer">
@@ -119,7 +140,7 @@
       <a href="{{url('residenciales')}}">CONDOMINIOS</a>
 			<a href="{{url('quienes-somos')}}">¿QUIÉNES SOMOS?</a>
 			<a href="{{url('legales')}}">LEGAL</a>
-			<a href="#">CONTACTO</a>
+			<a href="{{url('contacto')}}">CONTACTO</a>
     </div>
 
   </div>
