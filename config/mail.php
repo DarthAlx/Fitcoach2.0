b400cdff8ad604f750cf7f3e3993949ac5e618d0
Alexis Morales <alx.morales@hadoukendev.com>
2017-10-08 18:46:50 -0500

    retrieve

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'ventas@hadoukendev.com', 'name' => 'FITCOACH'],
 
     /*
     |--------------------------------------------------------------------------
