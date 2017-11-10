8ca2413508691c4e1124d8e8005105b000a0cbba
Alexis Morales <alx.morales@hadoukendev.com>
2017-11-09 21:53:14 -0600

    evento admin

--- resources/views/eventos.blade.php
+++ resources/views/eventos.blade.php
@@ -93,7 +93,7 @@
     												</div>
 														</div>
 														<div class="col-md-4 col-sm-12 sidebar hidden-xs hidden-sm">
-	    												<div class="title pull-right">
+	    												<div class="title pull-right" style="    line-height: 1;">
 	    													${{$evento->precio}}
 	    												</div>
 
