90094978468e73e66a056d1ef20365674785646f
Alexis Morales <alx.morales@hadoukendev.com>
2017-11-13 11:20:27 -0600

    smartlook

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -75,6 +75,15 @@ fbq('track', 'ViewContent');
 </noscript>
 <!-- End Facebook Pixel Code -->
 
+
+<script type="text/javascript">
+    window.smartlook||(function(d) {
+    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+    c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
+    })(document);
+    smartlook('init', '2dc9785490b72746428cc3d6e536dff9997e324a');
+</script>
 	</head>
 <body class="home">
 	<div class="pagecontainer">
