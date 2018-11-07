450f32a543df3af4b1062b5d1a62da9863133b13
Andresdkm <anfadimo@hotmail.com>
2018-11-07 15:14:19 -0500

    Renombrada migracion

--- database/migrations/2018_10_22_234326_alter_grupos2_table.php
+++ database/migrations/2018_10_22_234326_alter_grupos2_table.php
@@ -14,7 +14,6 @@ class AlterGrupos2Table extends Migration
 	{
 		Schema::table('grupos', function (Blueprint $table) {
 			$table->string('audiencia',100)->nullable()->change();
-			$table->date('deleted_at')->nullable();
 		});
 	}
 
@@ -26,7 +25,6 @@ class AlterGrupos2Table extends Migration
     public function down()
     {
 	    Schema::table('grupos', function (Blueprint $table) {
-		    $table->dropColumn('deleted_at');
 	    });
     }
 }
